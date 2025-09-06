5ddfc86940e21db4f3ef407e2be71af7328c9c7e
Jonathan <rochacontrerasjonathan40@gmail.com>
2025-09-05 22:43:00 -0400

    Agrege funcion saludar y funcion mostrarGenero

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
+
+fun saludar(saludo: String, nombreCompleto: String) {
+    var mensajeDefault: String = "Hola"
+
+    if(saludo.isBlank()) {
+        print(mensajeDefault)
+    } else {
+        print(saludo)
+    }
+    return println(" $nombreCompleto")
+}
+
+fun printGenero(genero: String) {
+    println("Mi genero es: $genero")
+}
+
+
 fun main() {
-    println("Hello World!")
+    saludar("","Jonathan")
+    printGenero("Helicoptero Apache H-64 de combate F22 raptor")
 }
